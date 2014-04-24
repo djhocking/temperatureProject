0dad04627d7ef45c4fb8fe00f32f4de99810f07e
Kyle O'Neil <kponeil314@gmail.com>
2014-04-24 09:56:30 -0400

    updated prcp values to reflect location corrections in ObservedClimateData files in dataIn folder

--- code/dataChecks/editSiteFeatureIDsAndDaymetData2.R
+++ code/dataChecks/editSiteFeatureIDsAndDaymetData2.R
@@ -20,8 +20,8 @@ library(maptools)     # loads sp library too
 library(chron)
 library(ncdf)
 
-baseDir <- 'C:/KPONEIL/GitHub/projects/'
-
+baseDir   <- 'C:/KPONEIL/GitHub/projects/'
+daymetDir <- 'F:/KPONEIL/SourceData/climate/DAYMET/unzipped/Daily'
 
 #Load the function that indexes daymet tiles based on a lat/lon point:
 source(paste0(baseDir, 'temperatureProject/code/functions/indexDaymetTileByLatLon.R'))
@@ -29,15 +29,11 @@ source(paste0(baseDir, 'temperatureProject/code/functions/indexDaymetTileByLatLo
 #                             Read in spatial data
 #==================================================================================================================
 
+sourceNames  <- c   ('CTDEP', 'MAFW', 'MAUSGS', 'NHFG', 'NHDES', 'MEDMR', 'USFS', 'VTFWS')
+
 proj4.NHD  <- "+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs"
 
-#proj4.Daymet <- "+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=lonlat +no_defs "
-#proj4.NHDLambert <- "+proj=lcc +lat_1=25 +lat_2=60 +lat_0=42.5 +lon_0=-100 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83  +units=m +no_defs "
-
-#Catchments <- readShapePoly ( "C:/KPONEIL/USGS/NHDPlusV2/Modified Versions/NENY_NHDCatchment_Lambert.shp", proj4string=CRS(proj4.NHDLambert))
 Catchments <- readShapePoly ( "C:/KPONEIL/USGS/NHDPlusV2/Modified Versions/NENY_NHDCatchment.shp", proj4string=CRS(proj4.NHD))
-
-dataSource <- "VTFWS"
 
 load(paste0(baseDir, 'temperatureProject/dataIn/delineatedCatchments/DelineatedCatchments_NHDPlus_NENY.RData'))
 DelineatedCatchmentsMaster <- NENYDelineatedCatchments
@@ -47,15 +43,40 @@ MasterLength <- length(DelineatedCatchmentsMaster)
 #                             Read in Site data
 #==================================================================================================================
 
-setwd(paste0(baseDir, 'temperatureProject/dataIn/', dataSource))
+for ( i in 1:length(sourceNames)){
+  
+  s <- read.csv(paste0('C:/KPONEIL/GitHub/projects/temperatureProject/dataIn/',sourceNames[i],'/siteChanges_', sourceNames[i], '.csv'))
+  
+  # Specific sites to change
+  s1 <- s[which(s$correctFeatureID > 1 | s$localCatchment > 0 ),]
+  
+  # Edit the "correctFeatureID" column to contain both changes and ones that will stay the same.
+  s1$correctFeatureID[s1$correctFeatureID == 1] <- s1$currentFeatureID[s1$correctFeatureID == 1]
+  
+  s2 <- s1[,c('site', 'agency', 'correctFeatureID', 'localCatchment')]
+  
+  # Don't do anything to the duplicate sites found in VTFWS
+  if( sourceNames[i] == 'VTFWS' ) { 
+    load(paste0('C:/KPONEIL/GitHub/projects/temperatureProject/dataIn/', sourceNames[i], '/covariateData_', sourceNames[i], '.RData')) 
+    s2 <- s2[s2$site %in% covariateData$site,]
+  
+  }
+  if ( i == 1 ) {siteList <- s2} else(siteList <- rbind(siteList, s2))
+}
 
-load(paste0("streamTempData_",   dataSource, ".RData"))
-load(paste0("covariateData_", dataSource, ".RData"))
+for ( i in 1:length(sourceNames)){
+  
+  load(paste0('C:/KPONEIL/GitHub/projects/temperatureProject/dataIn/',sourceNames[i],'/streamTempSitesObservedClimateData_', sourceNames[i], '.RData'))
+  
+  dat <- masterData[masterData$site %in% siteList$site,]
+  
+  if ( i == 1 ) {mast <- dat} else(mast <- rbind(mast, dat))
+}
 
-Sites <- unique(masterData$site)
+masterData <- mast
 
 #Daymet variables you want:
-Variables <- c("prcp")  #, "dayl", "srad", "swe", "tmax", "tmin", "vp")
+Variables <- c("prcp")
 
 stats <- data.frame(site = NA, SiteLat = NA, SiteLon = NA, VarLat = NA, VarLon = NA, MinDist = NA)
 
@@ -73,7 +94,7 @@ for (i in 1:numTiles){
   
   #Now open the NetCDF with the known location:
   #--------------------------------------------
-  NCDF <- open.ncdf(paste0("F:/KPONEIL/SourceData/Projected/DAYMET/Daily/", Tiles[i], "_2010/prcp.nc"))    #netcdf   
+  NCDF <- open.ncdf(paste0(daymetDir,'/', Tiles[i], "_2010/prcp.nc"))    #netcdf   
          
   #Dimension limits of each of the variables we'll use:
   #----------------------------------------------------
@@ -98,19 +119,21 @@ MasterCoordsMatrix <- MasterCoords
 MasterCoords <- as.data.frame(MasterCoords)
 
 #==================================================================================================================
-#                               Loop through Sites and NetCDFs, getting data.
+#                               Loop through siteList$site and NetCDFs, getting data.
 #==================================================================================================================
 
 start.time <- proc.time()[3]
-for ( i in 1:length(Sites)){
-    
+for ( i in 1:length(siteList$site)){
+
   print(i)
   
   #----------------------------------------------------------------------------------
   #Get the catchment polygon:
   #----------------------------------------------------------------------------------
-  featureID <- covariateData$FEATUREID[covariateData$site %in% Sites[i]]
-  features <- DelineatedCatchmentsMaster[[which(sapply(c(1:MasterLength),FUN = function(x){DelineatedCatchmentsMaster[[x]][1]==featureID})==TRUE)]]
+  featureID <- siteList$correctFeatureID[i]
+  
+  if (siteList$localCatchment[i] == 0 ){features <- DelineatedCatchmentsMaster[[which(sapply(c(1:MasterLength),FUN = function(x){DelineatedCatchmentsMaster[[x]][1]==featureID})==TRUE)]]
+    }else(features <- siteList$correctFeatureID[i])
   
   CatchmentShape <- Catchments[Catchments$FEATUREID %in% features,]
   BasinShape     <- gUnaryUnion(CatchmentShape) #dissolve individual catchments
@@ -119,14 +142,13 @@ for ( i in 1:length(Sites)){
   inside <- as.data.frame(a[!is.na(over(a, BasinShape)),])
   #----------------------------------------------------------------------------------
   
-  
   #----------------------------------------------------------------------------------
   #If no point falls within the catchment, find the nearest one.
   #----------------------------------------------------------------------------------
   if(nrow(inside) == 0 ){
-    
-    TempLat <- covariateData$Latitude [covariateData$site %in% Sites[i]]
-    TempLon <- covariateData$Longitude[covariateData$site %in% Sites[i]]
+       
+    TempLat <- coordinates(BasinShape)[2]
+    TempLon <- coordinates(BasinShape)[1]
     
     distances <- spDistsN1(MasterCoordsMatrix, c(TempLon, TempLat), longlat = TRUE)
     MinDist <- min(distances)
@@ -163,23 +185,21 @@ for ( i in 1:length(Sites)){
   SubTiles <- unique(SpatialLocs$Tile)
   #----------------------------------------------------------------------------------
   
-  
   #----------------------------------------------------------------------------------
   #Link record with catchment area
   #----------------------------------------------------------------------------------
-  CurRecord <- masterData[which(masterData$site == Sites[i]),]
+  CurRecord <- masterData[which(masterData$site == siteList$site[i]),]
   CurRecord <- CurRecord[which(CurRecord$year < 2013),]
-  
-  BegYr <- min(CurRecord$year)
-  EndYr <- max(CurRecord$year)
     
+  Yrs <- unique(CurRecord$year)
+  
   for (j in 1:length(Variables)){
     
-    for ( year in BegYr:EndYr ){
+    for ( year in Yrs ){
       
       for ( t in 1:length(SubTiles)){
   
-        NCDF <- open.ncdf(paste0("F:/KPONEIL/SourceData/Projected/DAYMET/Daily/", SubTiles[t], "_", year,"/", Variables[j], ".nc"))    #netcdf
+        NCDF <- open.ncdf(paste0(daymetDir, "/", SubTiles[t], "_", year,"/", Variables[j], ".nc"))    #netcdf
         #Dimension limits of each of the variables we'll use:
         #----------------------------------------------------
         start1 = c(1,1)
@@ -212,29 +232,53 @@ for ( i in 1:length(Sites)){
           if ( t == 1 & m == 1) {temp.var <- data.frame(year, dOY, var[position[1], position[2], 1:365])} else(temp.var <- cbind(temp.var, var[position[1], position[2], 1:365]))
           
         }
-      } 
+      }#end SubTile loop 
       
       ifelse( ncol(temp.var) > 3, R <- rowMeans(temp.var[,-c(1,2)], na.rm = FALSE, dims = 1),  R <- temp.var[,-c(1,2)] )
       
       temp.var <- data.frame(temp.var[,c(1,2)], R)
       names(temp.var) <- c("year", "dOY", paste0(Variables[j]))
       
-      if ( year == BegYr ) ( main.var <- temp.var)
-      if ( year >  BegYr ) ( main.var <- rbind(main.var, temp.var))
+      if ( year == Yrs[1] ) ( main.var <- temp.var)
+      if ( year >  Yrs[1] ) ( main.var <- rbind(main.var, temp.var))
 
       rm(temp.var, R)
-    }
+    }#end year loop
 
     if (j == 1) {all.vars <- main.var} else {all.vars <- merge(all.vars, main.var, by = c('year','dOY'), all.x = T)}  
-  }
+  }#end variable loop
 
   #Add data into main dataframe
-  all.vars$site <- Sites[i]
+  all.vars$site <- siteList$site[i]
   TempRecord <- merge(CurRecord, all.vars, by = c("site", "year", "dOY"), all.x = T)
   if (i == 1) {FullRecord <- TempRecord} else {FullRecord <- rbind(FullRecord, TempRecord)}
 }
 end.time   <- proc.time()[3]
 print(paste0((end.time-start.time)/3600, " hours"))
+
+save(FullRecord, file = paste0('C:/KPONEIL/temporary/FullRecord.RData'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
